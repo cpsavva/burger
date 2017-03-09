1452c2bfb1ea99c65b312b3b95746eff181cc97b
cpsavva <csoriasavva@gmail.com>
2017-03-08 22:54:25 -0500

    preliminary work on debugging display problems

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,26 @@
 
 const mysql = require('mysql');
 
-const mysqlconnect = mysql.createConnection({
+const mysqlconnect = function(app){
+
+
+var connection  = 
+mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "burgers_db"
 });
 
+/*connect to mysql */
+connection.connect(function(err) {
+  if (err) throw err;
 
+  console.log("connected as id " + connection.threadId);
+
+});
+
+};
 
 module.exports = mysqlconnect;
 
