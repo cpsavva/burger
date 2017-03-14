2a84ece5dccacd2864c253b21baa26a28da2413c
cpsavva <csoriasavva@gmail.com>
2017-03-13 21:20:42 -0400

    trying to debug heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,14 @@ const mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL){
-	connection = mysql.createConnection(process.env,JAWSDB_URL);
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {  
 	connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+	  host: "localhost",
+	  user: "root",
+	  password: "",
+	  database: "burgers_db"
+	});
 };
 
 /*connect to mysql */
